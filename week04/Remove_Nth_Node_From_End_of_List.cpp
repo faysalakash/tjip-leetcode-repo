27d66a3c0c33584a1d1031a4615cf85802790a3d
faysal.ahmed <faysal21akash@gmail.com>
2023-01-05 02:19:33 +0600

    Remove Nth Node From End of List

---
+++ week04/Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,35 @@
+// TC = O(1)
+// MC = O(1)
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+   public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        auto first = head;
+        auto second = head;
+
+        while (n--) {
+            first = first->next;
+        }
+
+        if (!first) return head->next;  // return NULL if there's only one element
+
+        while (first->next) {
+            first = first->next;
+            second = second->next;
+        }
+
+        auto nodeToDelete = second->next;
+        second->next = second->next->next;
+        delete (nodeToDelete);
+
+        return head;
+    }
+};
